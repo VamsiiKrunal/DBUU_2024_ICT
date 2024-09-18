53357b5bd60118e574b3e498f389424c44ebe92b
VamsiiKrunal <vamsiiguttikonda@gmail.com>
2024-09-18 12:47:56 +0530

    added post wala updation

--- springboot/datajpa/datajpa/src/main/java/com/controllers/StudentController.java
+++ springboot/datajpa/datajpa/src/main/java/com/controllers/StudentController.java
@@ -3,7 +3,12 @@ package com.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.Student;
@@ -19,5 +24,11 @@ public class StudentController {
 	public List<Student> getStudents(){
 		return service.getAllStudents();
 	}
-
+	
+	@PostMapping("/insertStudent")
+	public Student insertStudent(@RequestBody Student stobj) {
+		return service.insertStudent(stobj);
+		
+	}
+	
 }
--- springboot/datajpa/datajpa/src/main/java/com/service/StudentService.java
+++ springboot/datajpa/datajpa/src/main/java/com/service/StudentService.java
@@ -3,7 +3,9 @@ package com.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.entity.Student;
 import com.repository.StudentRepository;
@@ -18,4 +20,9 @@ public class StudentService {
 		return repo.findAll();
 		
 	}
+	public Student insertStudent(Student stuObj) {
+		return repo.save(stuObj);
+		
+	}
+	
 }
